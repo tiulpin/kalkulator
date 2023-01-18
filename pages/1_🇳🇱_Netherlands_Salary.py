3799f9a9200297ffe4ef950d86b5e1afc8474417
tiulpin <viktor@tiulp.in>
2023-01-18 01:02:34 +0100

    :children_crossing: Add tabs

--- pages/1_🇳🇱_Netherlands_Salary.py
+++ pages/1_🇳🇱_Netherlands_Salary.py
@@ -20,7 +20,8 @@ NO_TAXES_MESSAGE = (
 )
 EXPLANATIONS_TITLE = "The calculations are incorrect, what should I do?"
 EXPLANATIONS = """#### 🤔 What are the formulas and how the calculator works?
-The calculations are perfectly explained in 🔗[the article](https://www.iamexpat.nl/expat-info/taxation/dutch-tax-system).
+The calculations are perfectly explained in 
+[iamexpat.nl article](https://www.iamexpat.nl/expat-info/taxation/dutch-tax-system).
 
 
 #### 🤑 Your employee pays you some benefits or charges for something else
@@ -33,16 +34,17 @@ approximate your annual income.
 When you get your salary, you pay taxes based on your annual income (from the contract), but when you work only for 
 part of the year, your actual payment might be lower than your expected yearly income. 
 That is why you could get a refund from the tax services; you will have to fill in the tax declaration. 
-More information about filling out the declaration can be found 🔗[here](https://www.iamexpat.nl/expat-info/taxation/annual-dutch-tax-form).
+More information about filling out the declaration can be found in 
+[iamexpat.nl article](https://www.iamexpat.nl/expat-info/taxation/annual-dutch-tax-form).
 
 #### 🤷 The stupid developer made a mistake
-If you think that the calculator is wrong, please 🔗[drop me a message](https://t.me/tiulpin), and I will fix it as soon as possible.
+If you think that the calculator is wrong, please [drop me a message](https://t.me/tiulpin), and I will fix it as soon as possible.
 """
 DISCLAIMER = (
     "💡 **Disclaimer:** This is a demo app. The numbers may not be accurate. "
     "Consult a tax advisor for more information."
 )
-RULING_TIP = f"More information about the 30% ruling [🔗here]({RULING_URL})"
+RULING_TIP = f"More information about the 30% ruling 🔗[here]({RULING_URL})"
 
 
 def show_metrics(t: TaxesResult) -> None:
@@ -91,51 +93,61 @@ def show_table(t: TaxesResult) -> None:
     st.table(data.style.format("{:.2f}"))
 
 
-st.set_page_config(page_title="Netherlands Salary", page_icon="🇳🇱")
-st.title("🇳🇱 Netherlands Salary")
+st.set_page_config(page_title="Netherlands: Salary", page_icon="🇳🇱")
+st.title("🇳🇱 Netherlands: Salary")
 st.caption("Calculate how much money you get after the taxes")
 
-income_col, period_col = st.columns(2)
-income = income_col.number_input(
-    "Income in EUR", value=DEFAULT_SALARY, min_value=0.0, step=1000.0
-)
-period = period_col.selectbox("Period", WORKING_PERIODS)
-ruling = st.checkbox("30% facility", help=RULING_TIP, value=True)
-ruling_type = "Normal"
+tab_employed, tab_semployed = st.tabs(["Employed", "Self-employed"])
 
-with st.expander("Advanced options"):
-    first_col, second_col = st.columns(2)
-    year = first_col.selectbox("Year", list(reversed(NL_DATA["years"])))
-    hours = second_col.number_input(
-        "Weekly working hours", value=NL_DATA["defaultWorkingHours"], min_value=1
+with tab_employed:
+    income_col, period_col = st.columns(2)
+    income = income_col.number_input(
+        "Income in EUR", value=DEFAULT_SALARY, min_value=0.0, step=1000.0
     )
-    holiday_allowance_included = st.checkbox("Holiday allowance included", value=True)
-    social_security = st.checkbox("Social security", value=True)
-    old_age = st.checkbox("66 years or older", value=False)
-    if ruling:
-        ruling_type = st.selectbox("Ruling Type", RULING_TYPES.keys())
-    else:
-        ruling_type = "None"
+    period = period_col.selectbox("Period", WORKING_PERIODS)
+    ruling = st.checkbox("30% facility", help=RULING_TIP, value=True)
+    ruling_type = "Normal"
 
-calc = TaxCalculator(
-    salary=income,
-    old_age=old_age,
-    year=year,
-    ruling=ruling_type,
-    period=period,
-    working_hours=hours,
-    social_security=social_security,
-    holiday_allowance=holiday_allowance_included,
-    tax_data=NL_DATA,
-    working_periods=WORKING_PERIODS,
-)
-tax_results = calc.calculate()
-show_metrics(tax_results)
-if tax_results.year_net_income > 0:
-    show_table(tax_results)
-else:
-    st.success(NO_TAXES_MESSAGE)
-    st.balloons()
+    with st.expander("Advanced options"):
+        first_col, second_col = st.columns(2)
+        year = first_col.selectbox("Year", list(reversed(NL_DATA["years"])))
+        hours = second_col.number_input(
+            "Weekly working hours", value=NL_DATA["defaultWorkingHours"], min_value=1
+        )
+        holiday_allowance_included = st.checkbox("Holiday allowance included", value=True)
+        social_security = st.checkbox("Social security", value=True)
+        old_age = st.checkbox("66 years or older", value=False)
+        if ruling:
+            ruling_type = st.selectbox("Ruling Type", RULING_TYPES.keys())
+        else:
+            ruling_type = "None"
+
+    calc = TaxCalculator(
+        salary=income,
+        old_age=old_age,
+        year=year,
+        ruling=ruling_type,
+        period=period,
+        working_hours=hours,
+        social_security=social_security,
+        holiday_allowance=holiday_allowance_included,
+        tax_data=NL_DATA,
+        working_periods=WORKING_PERIODS,
+    )
+    tax_results = calc.calculate()
+    show_metrics(tax_results)
+    if tax_results.year_net_income > 0:
+        show_table(tax_results)
+    else:
+        st.success(NO_TAXES_MESSAGE)
+        st.balloons()
+
+with tab_semployed:
+    st.markdown("""
+    ### Coming soon...
+    
+    Meanwhile, you can watch the following relaxing video:""")
+    st.video("https://www.youtube.com/watch?v=3FMyeGw6Tn0")
 
 with st.expander(EXPLANATIONS_TITLE):
     st.markdown(EXPLANATIONS)
